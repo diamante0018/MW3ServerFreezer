a0e9f2c60b7d53dcf2a5f10a28a65dfd48ca478b
FutureRave <edoardo.sanguineti222@gmail.com>
2021-11-14 18:31:14 +0000

    More patches

--- src/component/remove_hooks.cpp
+++ src/component/remove_hooks.cpp
@@ -21,6 +21,12 @@ namespace remove_hooks
 			utils::hook::set<BYTE>(0x4E3D44, 0x25);
 			utils::hook::set<BYTE>(0x4E3D45, 0xFE);
 			utils::hook::set<BYTE>(0x4E3D46, 0xFF);
+
+			utils::hook::set<BYTE>(0x06EA960, 0x55);
+			utils::hook::set<BYTE>(0x06EA961, 0x8B);
+			utils::hook::set<BYTE>(0x06EA962, 0xEC);
+			utils::hook::set<BYTE>(0x06EA963, 0x81);
+			utils::hook::set<BYTE>(0x06EA964, 0xEC);
 		}
 	};
 }
